39fae25249c79d7500e0f493323378d31b79ef86
lupin4 <davidclabaugh@fullsail.edu>
2024-10-24 22:48:28 -0700

    Navbar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import NavigationProvider from "./context/navigation.jsx"
 
 const el = document.getElementById("root");
 const root = createRoot(el);
-root.render(<App />);
+root.render(<NavigationProvider><App /></NavigationProvider>);
